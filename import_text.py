31113947618f1da2e834391595a3167fe3a716e3
Qloeb <quentin.loeb@gmail.com>
2023-03-16 10:17:35 +0100

    import des données par paragraphes

--- import_text.py
+++ import_text.py
@@ -1,10 +1,24 @@
 from bs4 import BeautifulSoup
 
-with open("maupassant-fort.htm") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+import os
 
-paragraphes = soup.select("p")
-for p in paragraphes:
-    texte = p.get_text()
-    print(texte)
-    
+path = "data"
+textes = []
+labels = []
+
+for file in os.listdir(path):
+
+    path_file = os.path.join(path, file)
+
+    if os.path.isfile(path_file):
+
+        with open(path_file) as fp:
+            soup = BeautifulSoup(fp, "html.parser")
+
+        paragraphes = soup.select("p")
+        for p in paragraphes:
+            texte = p.get_text()
+            textes.append(texte)
+            labels.append(file)
+
+print("Nombre d'échantillons :",len(textes))
